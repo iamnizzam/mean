0e5e363b2586ae04037e6c832a4d85a08bf868fd
iamnizzam <aussieniki@gmail.com>
2016-12-21 18:23:25 +0600

    add cluster for performance

--- server.js
+++ server.js
@@ -3,5 +3,25 @@
 /**
  * Module dependencies.
  */
+var cluster = require('cluster'),
+     os = require('os');
+
 var app = require('./config/lib/app');
-var server = app.start();
+var numCPUs = os.cpus().length;
+
+
+if (cluster.isMaster) {
+
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+  
+ cluster.on('exit', function (worker) {
+   console.log('worker ' + worker.process.pid + ' died');
+    cluster.fork()
+ });
+  
+} else {
+    var server = app.start();
+}
+
